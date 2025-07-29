4b825f42382f5db7a2575753c881131f39b3f464
xp <nathan.han147@gmail.com>
2025-07-28 22:59:56 -0500

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -1,64 +1,77 @@
-/*
-SHOW databases; -- see which databases to use
+create database test;
 
--- USE database_name_to_use;
+use test;
 
-CREATE database test;
-USE test;
-show tables;
-
-CREATE TABLE ex(
-	id INT;
+create table addresses(
+	id INT,
+    house_number INT,
+    city VARCHAR(20),
+    postcode VARCHAR(7)
 );
 
-DROP TABLE ex;
-
-DROP database test
-*/
-
-SHOW DATABASES;
-
-CREATE DATABASE coffee_store;
-
--- which database to use
-USE coffee_store;
-
-CREATE TABLE products (
-	-- list of columns go inside here
-    id INT auto_increment PRIMARY KEY, -- primary key which is auto incremented
-    name VARCHAR(30),
-    price DECIMAL(3,2) -- 3 sig figs, 2 decimal
+create table people(
+	id INT,
+    first_name varchar(20),
+    last_name varchar(20),
+    address_id INT
 );
 
-SHOW TABLES; -- show tables in database
-
-create table customers(
-	id INT auto_increment PRIMARY KEY,
-    first_name varchar(30),
-    last_name varchar(30),
-	gender ENUM('M', "F"),
-    phone_number varchar(11)
+create table pets (
+	id INT,
+    name varchar(20),
+    species varchar(20),
+    owner_id INT
 );
 
-create table orders(
-	id INT auto_increment PRIMARY KEY,
-    product_id INT,
-    customer_id INT,
-    order_time DATETIME,
-    FOREIGN KEY (product_ID) REFERENCES products(id),
-    -- declare product id as foreign, it refers to the id col in products table (which must be primary key)
-    FOREIGN KEY (customer_id) REFERENCES customers(id)
-);
+-- add/remove primary key
+DESCRIBE addresses;
+ALTER table addresses ADD PRIMARY KEY (id);
+ALTER table addresses DROP PRIMARY KEY; -- when make priority, may not make null
+-- to change back to null
+ALTER table addresses MODIFY id int;
 
--- ---------------------
+DESCRIBE people;
+-- add foreign key
+ALTER TABLE people ADD CONSTRAINT FK_PeopleAddress FOREIGN KEY (address_id) REFERENCES addresses(id);
 
--- add/remove columns 
-DESCRIBE products;
+-- remove foreign key
+ALTER TABLE people DROP FOREIGN KEY FK_PeopleAddress, DROP INDEX FK_PeopleAddress;
 
-ALTER TABLE products -- alter table, NO SEMI COLON HERE
-ADD COLUMN coffee_origin VARCHAR(30); 
+-- unique constraint 
+DESCRIBE pets;
+ALTER TABLE pets
+ADD CONSTRAINT u_species UNIQUE (species);
+ALTER TABLE pets DROP INDEX u_species;
 
-ALTER TABLE products drop column coffee_origin; -- delete col
+-- rename col
+ALTER TABLE pets change species animal_type VARCHAR(20);
+ALTER TABLE pets rename column animal_type TO species;
+ALTER TABLE pets rename column `what species` TO species;
 
--- ---------------------
+-- change col data type
+DESCRIBE addresses;
+ALTER TABLE addresses modify column city int;
+ALTER TABLE addresses modify column city char(20);
 
+-- some fun
+-- add primary key to id field in pets table
+DESCRIBE pets;
+ALTER TABLE pets ADD PRIMARY KEY (id);
+
+-- add primary key to id field in people table
+DESCRIBE people;
+ALTER TABLE people ADD PRIMARY KEY (id);
+
+-- add foreign key to owner_id of pets, referencing id in people
+DESCRIBE pets;
+ALTER TABLE pets ADD CONSTRAINT FK_Pets FOREIGN KEY (owner_id) REFERENCES people(id);
+
+-- add col named email to people table
+ALTER TABLE people ADD COLUMN email varchar(20);
+ALTER TABLE people ADD CONSTRAINT u_email UNIQUE(email);
+
+-- rename name in pets to first_name
+ALTER TABLE pets RENAME COLUMN name TO first_name;
+
+-- change postcode data to char(7) in addresses
+ALTER TABLE addresses MODIFY COLUMN postcode char(7);
